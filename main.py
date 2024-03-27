3f0f9156d45e437f439410708d0a9da0c71880de
xRodrigoSx <rodrgosantos123@hotmail.com>
2024-03-27 11:54:32 -0300

    Create

---
+++ main.py
@@ -0,0 +1,42 @@
+import scrapy
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class Governo(scrapy.Spider):
+    name = "Dados do Governo"
+    start_urls = ["https://dados.gov.br/dados/conjuntos-dados"]
+
+    def parse(self, response):
+        SELETOR = ".search-result container my-2"
+        dados = []
+        for categoria in response.css(SELETOR):
+            dado = {}
+
+            NOME_SELETOR = ".capitalize.search-result-title.text-base.text-gray-900 ::text"
+
+            dado['nome'] = categoria.css(NOME_SELETOR).extract_first()
+            print(dado)
+
+            dados.append(dado)
+
+            driver = webdriver.Chrome()
+
+            driver.get("https://dados.gov.br/dados/conjuntos-dados")
+
+            pagina_pulada = pular_pagina(driver)
+
+            if pagina_pulada:
+                print("Página pulada com sucesso!")
+            else:
+                print("Erro ao pular página.")
+
+        print("Total de dados: ", len(dados))
+        
+        def pular_pagina(driver):
+            try:
+                proxima_pagina = driver.find_element(By.CSS_SELECTOR, "a.next-page")
+                proxima_pagina.click()
+                return True
+            except:
+                return False
+        
